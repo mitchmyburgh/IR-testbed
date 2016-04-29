56b9cd14db2d6f915e6782d4e2b19453dc4d49d1
Mitch Myburgh <mitchmyburgh@gmail.com>
2016-04-29 07:28:26 +0200

    added files

---
+++ gen_files.sh
@@ -0,0 +1,14 @@
+for ((i = 1; i<= 200; i++))
+do
+	touch ./for_submission/document.$i
+done
+
+for ((i = 1; i<= 5; i++))
+do
+	touch ./for_submission/query.$i
+done
+
+for ((i = 1; i<= 5; i++))
+do
+	touch ./for_submission/relevance.$i
+done
